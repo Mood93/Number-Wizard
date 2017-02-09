0abcbe2834ba582587bbf03d9cbbefea505b6a2d
Moudy <mood93@hotmail.com>
2017-02-09 01:36:04 -0600

    Added main game script

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+    // Use this for initialization
+    int max;
+	int min;
+    int guess;
+
+    int maxGuessesAllowed = 8;
+
+    public Text text;
+    public bool correctGuess = true;
+	
+	void Start () {
+        StartGame();
+	}
+
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower() {
+        max = guess;
+        NextGuess();
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        max = max + 1;
+    }
+
+     public void SetBool() {
+        correctGuess = false;
+    }
+
+
+    void NextGuess() {
+
+        guess = (max + min) / 2;
+        text.text = guess.ToString();
+        maxGuessesAllowed -= 1;
+        if (maxGuessesAllowed <= 0 && correctGuess == false) {
+            Application.LoadLevel("Win");
+        }
+        
+    }
+}
